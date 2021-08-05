8e2ee747ffa6a63ff53dae1e04769a80e8d3687a
Ades <adesbsp@gmail.com>
2021-08-05 15:56:10 +0700

    design register page
    design Register page

--- cloneGojek/src/pages/Register/index.js
+++ cloneGojek/src/pages/Register/index.js
@@ -1,39 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import {Colors} from '../../Utils';
-import { InputComp } from "../../components";
+import {InputComp} from '../../components';
+import IconBack from '../../images/back.png';
+import BannerIlustration from '../../images/assets.gif';
+
 
 const Register = () => {
   return (
-    <View style={Styles.wrapper}>
-      <View style={Styles.buttonBack} />
-      <View style={Styles.bannerIlustration}/>
-      <Text style={Styles.textHeader}> Lengkapi Data Berikut untuk Proses Registrasi</Text>
-      <InputComp placeholder={"Nama Lengkap"}/>
-      <InputComp placeholder={"Email"}/>
-      <InputComp placeholder={"Password"}/>
-    </View>
+    <ScrollView>
+      <View style={Styles.wrapper}>
+        <View>
+          <Image source={IconBack} style={Styles.buttonBack}/>
+        </View>
+        <View >
+          <Image source={BannerIlustration} style={Styles.bannerIlustration}/>
+        </View>
+        <Text style={Styles.textHeader}>
+          {' '}
+          Lengkapi Data Berikut untuk Proses Registrasi
+        </Text>
+        <View style={Styles.space(34)} />
+        <InputComp placeholder={'Nama Lengkap'} />
+        <View style={Styles.space(12)} />
+        <InputComp placeholder={'Email'} />
+        <View style={Styles.space(12)} />
+        <InputComp placeholder={'Password'} />
+      </View>
+    </ScrollView>
   );
 };
 export default Register;
 const Styles = StyleSheet.create({
-  textHeader:{
-    fontSize:14,
-    fontWeight:'bold',
+  space: value => {
+    return {
+      height: value,
+    };
+  },
+  textHeader: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  bannerIlustration: {
+    marginTop: 8,
+    height: 106,
+    width: 190,
+    
 
   },
-  bannerIlustration:{
-    marginTop:8,
-    height: 106,
-    width: 115,
-    backgroundColor:Colors.disable,
-  },
-  wrapper:{
-    padding:20,
+  wrapper: {
+    padding: 20,
   },
   buttonBack: {
     width: 25,
     height: 25,
-    backgroundColor: Colors.default,
   },
 });
